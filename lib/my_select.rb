ccf88ae3d6a297cef564d4ba8486465aa3ee8530
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-13 00:35:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  result = []
   i = 0
   while i < collection
-      result <<  yield(collection[i])
+      result << collection[i]  if yield(collection[i])
   i += 1
   end
  result
